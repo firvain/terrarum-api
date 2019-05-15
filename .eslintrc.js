37ca7600b399403a54f002e50c503a6448d5ce26
firvain <etsipis@terracognita.eu>
2019-05-15 17:36:08 +0300

    nodemon.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  "parserOptions": { "ecmaVersion": 6 },
+  "parserOptions": { "ecmaVersion": 8 },
   extends: ["eslint:recommended", "plugin:node/recommended", "plugin:prettier/recommended"],
   "rules": {
     "prettier/prettier": "error"
--- server.js
+++ server.js
@@ -1,28 +1,52 @@
 /* eslint-disable no-console */
 const express = require("express");
-const bodyParser = require("body-parser");
+require("dotenv-flow").config();
 const MongoClient = require("mongodb").MongoClient;
 const compression = require("compression");
-const { join } = require("path");
+// const { join } = require("path");
 const morgan = require("morgan");
 const app = express();
 const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || "local";
-
+const env = process.env.NODE_ENV;
 app.use(morgan("dev"));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-MongoClient.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true })
-  .then(function(db) {
-    // <- db as first argument
-    console.log(db);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
 // parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const db = async () => {
+  try {
+    const db = await MongoClient.connect(process.env.MONGOLAB_URI, {
+      useNewUrlParser: true
+    });
+    return db;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+// MongoClient.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true })
+//   .then(function(db) {
+//     // <- db as first argument
+//     console.log(db);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+const asyncMiddleware = fn => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
 app.use(compression());
-app.use(express.static(join(__dirname, "dist")));
 app.get("/", (req, res) => res.send(env));
+app.get(
+  "/create",
+  asyncMiddleware(async (req, res) => {
+    try {
+      await db();
+
+      res.send("connected");
+    } catch (error) {
+      console.log(error.message);
+      res.send(error.message);
+    }
+  })
+);
 app.listen(port, () => console.log(`Listening on port ${port}`));
